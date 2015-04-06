9297cbe4185ab20c73697db36949f63e1b55ba4e
Lezhi Qu <qlz@cs.brown.edu>
2015-04-06 11:29:00 -0400

    Implement Insertion sort

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -7,9 +7,21 @@ public class InsertionSort {
 	 * @param unsortedArray		an unsorted array of integers
 	 * @return					the input array, sorted least to greatest
 	 */
-	public static int[] insertionSort(int[] unsortedArray) {
+	public static int[] insertionSort(int[] num) {
 		//TODO: S2 implements insertion sort
-		
-		return null;
+		int j;                     // the number of items sorted so far
+     		int key;                // the item to be inserted
+     		int i;  
+
+     		for (j = 1; j < num.length; j++)    // Start with 1 (not 0)
+    		{
+           		key = num[ j ];
+           		for(i = j - 1; (i >= 0) && (num[ i ] < key); i--)   // Smaller values are moving up
+          		{
+                		num[ i+1 ] = num[ i ];
+          		}
+        		num[ i+1 ] = key;    // Put the key in its proper location
+     		}
+		return num;
 	}
 }
